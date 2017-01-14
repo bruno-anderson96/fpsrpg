2f9aa2fc5cbff6fbdb7307baa239b11aabedd551
Bruno Anderson <brunoanderson.macedorosendo@gmail.com>
2017-01-14 15:38:14 -0300

    Projneto iniciado

---
+++ src/main/java/org/fps/rpg/FpsrpgApplication.java
@@ -0,0 +1,12 @@
+package org.fps.rpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpsrpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpsrpgApplication.class, args);
+	}
+}
---
+++ src/test/java/org/fps/rpg/FpsrpgApplicationTests.java
@@ -0,0 +1,16 @@
+package org.fps.rpg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FpsrpgApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
