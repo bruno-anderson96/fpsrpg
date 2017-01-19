f127c9379645baa02769f9a83dd9cc91c12df3c0
caveiraman <brunoanderson.macedorosendo@gmail.com>
2017-01-19 20:23:56 -0300

    Inciando controllers

--- src/main/java/org/fps/Controller/HomeController.java
+++ src/main/java/org/fps/Controller/HomeController.java
@@ -1,5 +1,21 @@
 package org.fps.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
 
+	@RequestMapping("/")
+	public String home(){
+		return "home";	
+	}
+	
+	@RequestMapping("/cadastroUser")
+	public String cadUsuario(){
+		return "cadastroUser";
+	}
+	
+	
+	
 }
