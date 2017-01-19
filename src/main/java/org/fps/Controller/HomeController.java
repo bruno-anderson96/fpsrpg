5810cc18fdcaf1782a59a70228476df382fc0051
caveiraman <brunoanderson.macedorosendo@gmail.com>
2017-01-19 20:10:19 -0300

    Configurando banco e templates

---
+++ src/main/java/org/fps/Controller/HomeController.java
@@ -0,0 +1,5 @@
+package org.fps.Controller;
+
+public class HomeController {
+
+}
--- src/main/java/org/fps/rpg/FpsrpgApplication.java
+++ src/main/java/org/fps/rpg/FpsrpgApplication.java
@@ -2,6 +2,11 @@ package org.fps.rpg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 public class FpsrpgApplication {
@@ -9,4 +14,20 @@ public class FpsrpgApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FpsrpgApplication.class, args);
 	}
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+		templateResolver.setTemplateMode("XHTML");
+		templateResolver.setPrefix("views/");
+		templateResolver.setSuffix(".html");
+
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver);
+
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(engine);
+		return viewResolver;
+	}
+
 }
