81be55fac81cc40317fce91cec6b6a534f49e376
caveiraman <brunoanderson.macedorosendo@gmail.com>
2017-01-19 20:06:05 -0300

    Adicionado repositorio granadas

---
+++ src/main/java/org/fps/Repository/ExplosivaRepository.java
@@ -0,0 +1,8 @@
+package org.fps.Repository;
+
+import org.fps.EntidadesItens.Explosiva;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExplosivaRepository extends JpaRepository<Explosiva, Long>{
+
+}
